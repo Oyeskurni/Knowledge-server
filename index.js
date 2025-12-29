4ab2fa0b6576eb0d45c56d453a06eb3059bc3764
oyeskurni <mdoyeskuruni72@gmail.com>
2025-12-29 23:01:16 +0600

    like unlike api create

--- index.js
+++ index.js
@@ -26,20 +26,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      const articlesCollection = client.db('articles').collection('article');
+    await client.connect();
+    const articlesCollection = client.db('articles').collection('article');
+    const commentsCollection = client.db('articles').collection('article_comments');
 
-      app.get('/articles', async (req, res) => {
-          const cursor = articlesCollection.find();
-          const result = await cursor.toArray();
-          res.send(result);
-      })
-    
+    app.get('/articles', async (req, res) => {
+      const cursor = articlesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.get('/articles/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await articlesCollection.findOne(query);
-        res.send(result);
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await articlesCollection.findOne(query);
+      res.send(result);
     })
 
     app.get('/articles', async (req, res) => {
@@ -49,14 +50,18 @@ async function run() {
       res.send(result);
     })
 
-      app.post('/articles', async (req, res) => {
-        const article = req.body;
-        console.log(article);
-        
-          const result = await articlesCollection.insertOne(article);
-          res.send(result);
-      })
-    
+    app.post('/articles', async (req, res) => {
+      const article = req.body;
+      const newArticle = {
+        ...article,
+        likes: [],
+        likesCount: 0,
+        createdAt: new Date()
+      };
+      const result = await articlesCollection.insertOne(newArticle);
+      res.send(result);
+    })
+
     app.delete('/articles/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -72,6 +77,66 @@ async function run() {
       const result = await articlesCollection.updateOne(query, updateDoc);
       res.send(result);
     })
+    app.patch('/articles/like/:id', async (req, res) => {
+      const articleId = req.params.id;
+      const { userId } = req.body;
+
+      const query = { _id: new ObjectId(articleId) };
+      const article = await articlesCollection.findOne(query);
+
+      if (!article) {
+        return res.status(404).send({ message: "Article not found" });
+      }
+
+      let updateDoc;
+
+      if (article.likes.includes(userId)) {
+        // UNLIKE
+        updateDoc = {
+          $pull: { likes: userId },
+          $inc: { likesCount: -1 }
+        };
+      } else {
+        // LIKE
+        updateDoc = {
+          $addToSet: { likes: userId },
+          $inc: { likesCount: 1 }
+        };
+      }
+
+      const result = await articlesCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+
+    app.post('/comments', async (req, res) => {
+      const comment = req.body;
+      console.log(comment);
+      const result = await commentsCollection.insertOne(comment);
+      res.send(result);
+    })
+
+    app.get('/comments', async (req, res) => {
+      const { articleId } = req.query;
+
+      let query = {};
+
+      if (articleId) {
+        query = { articleId: new ObjectId(articleId) };
+      }
+
+      const result = await commentsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+    app.get('/comments/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await commentsCollection.findOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
@@ -87,9 +152,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from server');
+  res.send('Hello from server');
 })
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
+  console.log('Listening to port', port);
 })
