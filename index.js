c120f1373c748c57ee5d0fb38c22278345e96be7
Oyeskuruni islam <157284689+Oyeskurni@users.noreply.github.com>
2026-01-09 20:42:48 +0600

    Update CORS origin to production URL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://knowsphere-ef061.web.app'],
   credentials: true
 }));
 app.use(express.json());
