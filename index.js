657a6b333aa8e155e3ee92ad81af7676d6aef9e8
Oyeskuruni islam <157284689+Oyeskurni@users.noreply.github.com>
2026-01-10 08:11:35 +0600

    Comment out CORS options middleware
    Comment out the CORS options middleware.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 }));
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser());
